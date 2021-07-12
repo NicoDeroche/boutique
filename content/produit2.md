d49216e510e84e44db5db5890b0b6fb33dce0cd9
NicoDeroche <nico.deroche@gmail.com>
2021-07-12 18:46:48 +0200

    produits

--- content/produit2.md
+++ content/produit2.md
@@ -1,7 +1,7 @@
 ---
 name: Produit 2
 description: fabrication Française
-price: 13000
+price: 130
 ---
 
  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum pharetra ultrices. Mauris leo diam, commodo sit amet maximus id, faucibus et lorem. Donec luctus euismod pellentesque. Proin massa arcu, interdum non sapien non, dignissim convallis mauris. Praesent auctor turpis sed ultrices venenatis. Mauris at vehicula diam. Sed vitae dictum ex, nec sagittis ante. Quisque blandit tortor nibh, vitae sodales lorem bibendum et. Nullam et hendrerit est.
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,48 @@
 import matter from "gray-matter";
+import Link from "next/link";
+import fs from "fs";
 
 const HomePage = (props) => {
- 
+
   console.log(props);
-  return (
-    <div>
-      <h1>
-        Bienvenue dans ma boutique
-      </h1>
-    </div>
-  )
+   return props.products.map(product => {
+    return (
+      <div>
+        <Link href={product.slug}>
+          <a>
+            <h1>{product.name}</h1>
+          </a>
+        </Link>
+        <p>{product.description}</p>
+        <p>{product.price} €</p>
+      </div>
+    )
+  }) 
 }
 
 
-export const getStaticProcs = async () => {
-  console.log("ok");
+
+
+export const getStaticProps = async () => {
   const directory = `${process.cwd()}/content`;
   const filenames = fs.readdirSync(directory);
   const products = filenames.map(filename => {
     const fileContent = fs.readFileSync(`${directory}/${filename}`).toString();
     const { data } = matter(fileContent);
-    const slug = `/products/${filename.replace('.md','')}`;
+    const slug = `/products/${filename.replace('.md', '')}`;
     return {
       ...data,
       slug
     }
   });
+  console.log(products);
   return {
-    props: { products  }
+    props: {
+    products
+    },
   }
-
 }
+
 
 
 export default HomePage;
