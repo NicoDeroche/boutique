0e00495ddb2325c242f2b1a20699ba1057e5c71b
NicoDeroche <nico.deroche@gmail.com>
2021-07-12 09:27:33 +0200

    Create products.js

---
+++ pages/products.js
@@ -0,0 +1,15 @@
+const Products = () =>
+{
+<>
+<h1>Cool product 1</h1>
+<p>I love it</p>
+<p>5.5€</p>
+<hr/>
+<h1>Cool product 2</h1>
+<p>I love it too</p>
+<p>6.5€</p>
+</>
+
+}
+
+export default Products;
